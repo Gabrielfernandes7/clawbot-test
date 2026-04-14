d198650278e81647bc87d371358b0b03dd7dbc0c
Gabriel Fernandes <gabrielassuncaofernandes@gmail.com>
2026-04-14 14:54:55 -0300

    feat: add docker command instal for ollama model

--- cmd/crabe/main.go
+++ cmd/crabe/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Gabrielfernandes7/crabe/internal/install"
 	"github.com/Gabrielfernandes7/crabe/internal/uninstall"
 	"github.com/Gabrielfernandes7/crabe/internal/setup"
+	"github.com/Gabrielfernandes7/crabe/internal/start"
 	"github.com/Gabrielfernandes7/crabe/internal/ui"
 	"github.com/spf13/cobra"
 )
@@ -28,6 +29,7 @@ func init() {
 	rootCmd.AddCommand(install.NewInstallCmd())
 	rootCmd.AddCommand(setup.NewSetupCmd())
 	rootCmd.AddCommand(uninstall.NewUninstallCmd())
+	rootCmd.AddCommand(start.NewStartCmd())
 }
 
 func main() {
